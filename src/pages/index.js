0369f720680eb7f423bf528045cff98c148f378c
peterjewicz <peterjewicz@totalwebconnections.com>
2022-01-08 12:41:37 -0600

    Startups section

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ const BlogIndex = ({ data, location }) => {
         >
         </div>
         <div className="text-white pt-28 max-w-2xl mx-auto">
-          <h2 className="text-4xl">My Startups</h2>
+          <h2 className="text-4xl">Startups</h2>
           <p>
             Always busy, over the years I've worked on a number of startup projects. Below, find
             the ones still kicking and see where I've worked.
@@ -87,33 +87,39 @@ const BlogIndex = ({ data, location }) => {
         </div>
       </div>
       <div className="py-28 px-4 md:flex max-w-5xl mx-auto">
-        <div className="px-2">
+        <div className="px-2 text-center md:text-left text-white">
           <h3 className="text-4xl">Total Web Connections</h3>
-          <p>Software development company. Currently working on a mix of client and internal projects.</p>
-          <a href="#"><button>Visit</button></a>
+          <p className="mb-8">Software development company. Currently working on a mix of client and internal projects.</p>
+          <div  className="mb-16">
+            <a href="#"><button>Visit</button></a>
+          </div>
         </div>
         <div className="px-2">
-          <img src={Totalwebconnections} width="640px" alt="Total Web Connections" />
+          <img className="mx-auto" src={Totalwebconnections} width="640px" alt="Total Web Connections" />
         </div>
       </div>
-      <div className="py-28 px-4 md:flex max-w-5xl mx-auto">
+      <div className="py-28 px-4 flex flex-col-reverse md:flex-row max-w-5xl mx-auto">
         <div className="px-2">
-          <img src={Mellowgolemgames} width="340px" alt="Mellow Golem Games" />
+          <img className="mx-auto" src={Mellowgolemgames} width="340px" alt="Mellow Golem Games" />
         </div>
-        <div className="px-2">
+        <div className="px-2 text-center md:text-right">
           <h3 className="text-4xl">Mellow Golem Games</h3>
-          <p>Game development and related content. Currently working on RPG games.</p>
-          <a href="#"><button>Visit</button></a>
+          <p className="mb-8">Game development and related content. Currently working on RPG games.</p>
+          <div  className="mb-16">
+            <a href="#"><button>Visit</button></a>
+          </div>
         </div>
       </div>
-      <div className="py-28 px-4 md:flex max-w-5xl mx-auto">
-        <div className="px-2">
+      <div className="py-28 px-4 md:flex max-w-5xl mx-auto border-b-2 border-primary">
+        <div className="px-2 text-center md:text-left">
           <h3 className="text-4xl">Nitor Fitness</h3>
-          <p>Fitness software offering customized workouts tailored to individuals.</p>
-          <a href="#"><button>Visit</button></a>
+          <p className="mb-8">Fitness software offering customized workouts tailored to individuals.</p>
+          <div  className="mb-16">
+            <a href="#"><button>Visit</button></a>
+          </div>
         </div>
         <div className="px-2">
-          <img src={NitorFitness} width="340px" alt="Total Web Connections" />
+          <img className="mx-auto" src={NitorFitness} width="340px" alt="Total Web Connections" />
         </div>
       </div>
 
