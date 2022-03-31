2b4f8b1ca07427a6e21d0713e6e68e754b326bd2
peterjewicz <peterjewicz@totalwebconnections.com>
2022-03-31 11:33:04 -0500

    add ga tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,19 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-653NEF35MV",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: true,
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
